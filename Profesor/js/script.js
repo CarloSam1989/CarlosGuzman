85a2bb7ec9ceeb3d1fb535072d7412194a72e261
CarlosSam1989 <carlosgh89@hotmail.com>
2023-05-08 20:14:16 -0500

    Borrar espacios

--- Profesor/js/script.js
+++ Profesor/js/script.js
@@ -4,15 +4,12 @@ const body = document.querySelector('body'),
     searchBtn = body.querySelector(".search-box"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText=body.querySelector(".mode-text");
-
     toggle.addEventListener("click", ()=> {
         sidebar.classList.toggle("close");
     })
-
     searchBtn.addEventListener("click", ()=> {
         sidebar.classList.remove("close");
     })
-
     modeSwitch.addEventListener("click", ()=> {
         body.classList.toggle("dark");
         if(body.classList.contains("dark")){
