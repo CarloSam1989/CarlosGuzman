5393ee660cb3efab300d9e664309578a56c943c0
CarlosSam1989 <carlosgh89@hotmail.com>
2023-05-17 17:47:40 -0500

    login

--- Profesor/js/login.js
+++ Profesor/js/login.js
@@ -1,9 +1,12 @@
 function login(){
-    const Swal = require('sweetalert2')
     var correo,pass
     correo=document.getElementById("email").value;
     pass=document.getElementById("password").value;
-    if(correo=="carlosgh89@hotmail.com"&&pass=="123456"){
+    if(correo=="" && pass=="")
+    {
+        alert("No deje campos en blanco");
+       
+    }else if(correo=="carlosgh89@hotmail.com" && pass=="123456"){
         window.location("menu.html");
     }else{
        alert("Datos incorrectos");
