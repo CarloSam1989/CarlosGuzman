8959281b037a849b4a8f00317c3e71838684effb
Sneyder_12 <jesustop188@gmail.com>
2023-05-17 21:09:18 -0500

    ajjajaa

--- SneyderJet/js/formulario.js
+++ SneyderJet/js/formulario.js
@@ -1,6 +1,5 @@
 const movPag= document.querySelector(".movPag");
 const btn_adelante2= document.querySelector(".sigPag");
-
 const btn_atras1= document.querySelector(".volver-pag1");
 const btn_adelante3= document.querySelector(".adelante-pag3");
 const btn_atras2= document.querySelector(".volver-pag2");
@@ -16,7 +15,6 @@ let max=4;
 let cont=1;
 
 
-
 btn_adelante2.addEventListener("click",function(e){
     var nombre = document.getElementById("nombres").value;
     var ape = document.getElementById("apellidos").value;
@@ -24,64 +22,32 @@ btn_adelante2.addEventListener("click",function(e){
     if(nombre=="" || ape=="" || iden==""){
       /* document.getElementById("nombres-error").innerHTML = "*Esto no puede estar vacío"; */
        alert("No puede quedar vacío.");
+       return true;
     }else{
         e.preventDefault();
         movPag.style.marginLeft="-25%"
         num[cont - 1].classList.add("active");
         progressText[cont - 1].classList.add("active");
         progressCheck[cont - 1].classList.add("active");
-        cont += 1;
+        index += 1;
     }
-    
 });
 btn_adelante3.addEventListener("click",function(e){
-    e.preventDefault();
-    num[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    cont += 1;
-    movPag.style.marginLeft="-50%"
-});
-btn_adelante4.addEventListener("click",function(e){
-    e.preventDefault();
-    num[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    cont += 1;
-    movPag.style.marginLeft="-75%"
-});
-btn_final.addEventListener("click",function(e){
-    e.preventDefault();
-    num[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    cont += 1;
-    alert("Fin de registro");
-});
-btn_atras1.addEventListener("click",function(e){
-    e.preventDefault();
-    movPag.style.marginLeft="0%"
-    num[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    cont -= 1;
+    var fecha = document.getElementById("fehcanac").value;
+    var sex = document.getElementById("sexo").value;
+    if(fecha=="" || sex== ""){
+      /* document.getElementById("nombres-error").innerHTML = "*Esto no puede estar vacío"; */
+       alert("No puede quedar vacío.");
+       return true;
+    }else{
+        e.preventDefault();
+        movPag.style.marginLeft="-25%"
+        num[cont - 1].classList.add("active");
+        progressText[cont - 1].classList.add("active");
+        progressCheck[cont - 1].classList.add("active");
+        index += 1;
+    }
 });
 
-btn_atras2.addEventListener("click",function(e){
-    e.preventDefault();
-    movPag.style.marginLeft="-25%"
-    num[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    cont -= 1;
-});
 
-btn_atras3.addEventListener("click",function(e){
-    e.preventDefault();
-    movPag.style.marginLeft="-50%"
-    num[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    cont -= 1;
-});
 
