5727c1ca429e6fd6f40624a969f3a0bc7b20531a
PRACTICA232 <bakkahosto@gufum.com>
2023-05-17 19:37:16 -0500

    hola

--- JoelRojas3/validacion.js
+++ JoelRojas3/validacion.js
@@ -3,11 +3,9 @@ function login (){
     correo=document.getElementById('email').value; 
     pass=document.getElementById('password').value;
     if (correo == "regaloyt334@gmail.com" && pass == "12345"){
-        window.location("menu.html");
-
+        window.location = "menu.html";
     }else{
         alert("datos incorrectos");
     }
-
 }
 
