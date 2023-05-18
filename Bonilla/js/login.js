a35f7603c7d12f952b1ce7924305595c4b7e183a
Bonishas <randybonil2222@gmail.com>
2023-05-17 19:39:28 -0500

    Monas Chinas

---
+++ Bonilla/js/login.js
@@ -0,0 +1,13 @@
+function login(){
+    var correo,pass
+    correo=document.getElementById("email").value;
+    pass=document.getElementById("password").value;
+    if(correo=="carlosgh89@hotmail.com" && pass=="123456"){
+        
+        window.location = "menu.html";
+        return true;
+    }else{
+       alert("Datos incorrectos");
+       return false;
+    }
+}
