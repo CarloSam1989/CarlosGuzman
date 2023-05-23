1c541c48b1b52a07391313392489313d1e01c80f
JhonnySarango <sarangosanchezj@outlook.com>
2023-05-22 19:59:58 -0500

    cambios

--- Jhonny Sarango/js/login.js
+++ Jhonny Sarango/js/login.js
@@ -2,13 +2,11 @@ function login(){
     var correo,pass
     correo=document.getElementById("email").value;
     pass=document.getElementById("password").value;
-    if(correo=="" && pass=="")
-    {
-        alert("No deje campos en blanco");
-       
-    }else if(correo=="sarangosanchezj@gmail.com" && pass=="Jhonny_123"){
-        window.location("menu.html");
+    if(correo=="sarangosanchezj@gmail.com" && pass=="123456"){
+        window.location = "menu.html";
+        return true;
     }else{
        alert("Datos incorrectos");
+       return false;
     }
 }
